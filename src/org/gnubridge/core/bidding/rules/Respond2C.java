4a646a0712e962714570a7560db9c24c1f867fef
AlexanderMisel <a6990891@gmail.com>
2014-08-15 15:10:45 +0800

    Initialize

---
+++ src/org/gnubridge/core/bidding/rules/Respond2C.java
@@ -0,0 +1,61 @@
+package org.gnubridge.core.bidding.rules;
+
+import org.gnubridge.core.Hand;
+import org.gnubridge.core.bidding.Auctioneer;
+import org.gnubridge.core.bidding.Bid;
+import org.gnubridge.core.bidding.PointCalculator;
+import org.gnubridge.core.deck.Diamonds;
+import org.gnubridge.core.deck.Suit;
+import org.gnubridge.core.deck.Clubs;
+import org.gnubridge.core.deck.Hearts;
+import org.gnubridge.core.deck.NoTrump;
+import org.gnubridge.core.deck.Spades;
+
+public class Respond2C extends Response {
+
+	private final PointCalculator pc;
+	public static final Suit[] mmlist = { Hearts.i(), Spades.i(), Clubs.i(), Diamonds.i() };
+
+	public Respond2C(Auctioneer a, Hand h) {
+		super(a, h);
+		pc = new PointCalculator(hand);
+	}
+
+	@Override
+	protected Bid prepareBid() {
+		Bid result = null;
+		if (pc.getHighCardPoints() >= 8) {
+			if (pc.isBalanced()) {
+				result = new Bid(2, NoTrump.i());
+			} else {
+				Suit longest = null;
+				for (Suit color : mmlist) {
+					if (hand.getSuitLength(color) >= 5) {
+						if (longest == null) {
+							longest = color;
+						} else if (hand.getSuitLength(color) > hand
+								.getSuitLength(longest)) {
+							longest = color;
+						}
+					}
+				}
+				if (longest != null) {
+					if (longest.isMajorSuit()) {
+						result = new Bid(2, longest);
+					} else {
+						result = new Bid(3, longest);
+					}
+				}
+			}
+			result.makeGameForcing();
+		} else {
+			result = new Bid(2, Diamonds.i());
+		}
+		return result;
+	}
+
+	@Override
+	protected boolean applies() {
+		return super.applies() && new Bid(2, Clubs.i()).equals(partnersOpeningBid);
+	}
+}
---
+++ src/org/gnubridge/core/bidding/rules/Strong2C.java
@@ -0,0 +1,28 @@
+package org.gnubridge.core.bidding.rules;
+
+import org.gnubridge.core.Hand;
+import org.gnubridge.core.bidding.Auctioneer;
+import org.gnubridge.core.bidding.Bid;
+import org.gnubridge.core.bidding.PointCalculator;
+import org.gnubridge.core.deck.Clubs;
+
+public class Strong2C extends BiddingRule {
+
+	private PointCalculator pc;
+
+	public Strong2C(Auctioneer a, Hand h) {
+		super(a, h);
+		pc = new PointCalculator(hand);
+	}
+
+	@Override
+	protected Bid prepareBid() {
+		return new Bid(2, Clubs.i());
+	}
+
+	@Override
+	protected boolean applies() {
+		return auction.isOpeningBid() && pc.getHighCardPoints() >= 22;
+	}
+
+}
